ad74d840cd42e54a4c323ad880a1859a549441fe
Mitchell <soggybag@gmail.com>
2020-06-03 15:46:25 -0700

    Updates

---
+++ precache-manifest.c6de82496f63da28ec7fb9d11818b2d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "8eac3f33b29f2dd07a65",
+    "url": "/react-tutorial/static/js/main.8eac3f33.chunk.js"
+  },
+  {
+    "revision": "d541d4ec350d9d2c8b4f",
+    "url": "/react-tutorial/static/js/1.d541d4ec.chunk.js"
+  },
+  {
+    "revision": "8eac3f33b29f2dd07a65",
+    "url": "/react-tutorial/static/css/main.ec25b864.chunk.css"
+  },
+  {
+    "revision": "755a092ec42f42471911d4feb75c6fdd",
+    "url": "/react-tutorial/index.html"
+  }
+];
